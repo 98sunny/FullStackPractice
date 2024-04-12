356c895aee4cbd59b5adfd8780e4be6b38c49fd5
Sarbjot <sarbjot@Sarbjots-MacBook-Air.local>
2024-04-12 19:49:45 -0400

    rebase completed

--- week6/hooks_concepts/hooks-demo/src/App.jsx
+++ week6/hooks_concepts/hooks-demo/src/App.jsx
@@ -17,6 +17,41 @@ function App() {
 
   return (
     <div>
+      <h1>Ths is a dumy feature added by Sarb</h1>
+    <h2>Yograj singh changed this line</h2>
+    <h1>This is a line added by Sunny</h1>
+    <h2>This is a dummy feature in the master branch</h2>
+    <h2>This is 2nd dummy feature in the master branch</h2>
+    {todos.map(todo=><Todo key={todo.id} title={todo.title} description={todo.description} />)}
+    </div>
+  )
+}
+function Todo({title,description}){
+  return <div>
+  <h1>{title} and description is {description}</h1> 
+  </div>
+}
+
+export default App
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  useEffect(() =>{
+    setInterval(() => {
+      fetch("https://sum-server.100xdevs.com/todos")
+    .then(async function(response) {
+      const json=await response.json();
+      setTodos(json.todos);
+    })
+    }, 2000);
+
+    
+  },[])
+
+  return (
+    <div>
 <<<<<<< HEAD
 <<<<<<< HEAD
 =======
